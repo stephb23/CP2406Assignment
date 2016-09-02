03d214364b220236e03d1dd8c6b01d2bbe386262
Steph Baker <stephanie.baker@my.jcu.edu.au>
2016-09-02 10:13:30 +1000

    Added Card class

---
+++ src/Card.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Stephanie on 2/09/2016.
+ */
+public class Card {
+    String imageFile, imageName, title;
+
+    public Card(String imageFile, String imageName, String title) {
+        this.imageFile = imageFile;
+        this.imageName = imageName;
+        this.title = title;
+    }
+}
---
+++ src/PlayCard.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Stephanie on 2/09/2016.
+ */
+public class PlayCard extends Card {
+    String imageFile, imageName, title, chemistry, classification, crystalSystem;
+    ArrayList<String> occurences = new ArrayList<>();
+    String hardness, specificGravity, cleavage, crystalAbundances, economicValue;
+
+
+}
