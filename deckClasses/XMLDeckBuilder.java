5b8559a8113c1dad06c68c6216a9bb4dc3efc4c2
Steph Baker <stephanie.baker@my.jcu.edu.au>
2016-09-05 22:58:42 +1000

    Created Deck class and DeckBuilder interface, as well as an XMLDeckBuilder class that implements the aforementioned interface. Progress is being made on XML parsing, with the file successfully being read in currently.

--- deckClasses/XMLDeckBuilder.java
+++ deckClasses/XMLDeckBuilder.java
@@ -1,8 +1,13 @@
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 import java.io.File;
 
 /**
@@ -12,14 +17,26 @@ public class XMLDeckBuilder implements DeckBuilder{
 
     public static void main(String[] args) {
         try {
+            // Read our XML file into a Java Document
             File xmlFile = new File("MstCards_151021.plist");
             DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
             Document document = documentBuilder.parse(xmlFile);
-            document.getDocumentElement().normalize();
-            System.out.println("Root element: " + document.getDocumentElement().getNodeName());
-            NodeList nodeList = document.getElementsByTagName("key");
-            System.out.println(nodeList.item(0).getTextContent());
+
+            // These are the nodes you're looking for *jedi wave*
+            // Item 0 in this list is a write-off; it's the whole bloody file.
+            // Last three items are game info cards, not playing cards. IGNORE.
+            NodeList nodeList = document.getElementsByTagName("dict");
+            for (int i = 1; i < nodeList.getLength() - 3; ++i) {
+                Node node = nodeList.item(i);
+                System.out.println("Node: " + node.getTextContent());
+                if (node.getNodeType()== Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    System.out.println(element.getElementsByTagName("string").item(2).getTextContent());
+                }
+            }
+
+
 
         } catch (Exception e) {
             System.out.println("Error reading the XML File");
